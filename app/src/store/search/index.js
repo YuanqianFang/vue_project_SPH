296c6e7abed19fd09b025e558c880473eb3ffb37
YuanqianFang <richardfang001105@gmail.com>
2022-05-09 17:38:11 +0800

    Search module
    搜索模块数据调取与用query params搜索的实现

--- app/src/store/search/index.js
+++ app/src/store/search/index.js
@@ -1,30 +1,39 @@
-import { reqGetSearchInfo } from "@/api"
+import { reqGetSearchInfo } from "@/api";
 const state = {
-    searchList:{},
-}
+  searchList: {},
+};
 
 const mutations = {
-    GETSEARCHLIST(state,searchList){
-        state.searchList = searchList
-    }
-}
+  GETSEARCHLIST(state, searchList) {
+    state.searchList = searchList;
+  },
+};
 
 const actions = {
-    async getSearchList({commit},params={}){
-        let result = await reqGetSearchInfo(params)
-        if(result.code == 200){
-            commit("GETSEARCHLIST",result.data)
-        }
+  async getSearchList({ commit }, params = {}) {
+    let result = await reqGetSearchInfo(params);
+    if (result.code == 200) {
+      commit("GETSEARCHLIST", result.data);
     }
-}
+  },
+};
 
+//getters主要作用是 简化仓库中的数据
 const getters = {
-    
-}
+  goodsList(state) {
+    return state.searchList.goodsList || [];
+  },
+  trademarkList(state) {
+    return state.searchList.trademarkList || [];
+  },
+  attrsList(state) {
+    return state.searchList.attrsList || [];
+  },
+};
 
-export default{
-    state,
-    mutations,
-    actions,
-    getters
-}
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+};
