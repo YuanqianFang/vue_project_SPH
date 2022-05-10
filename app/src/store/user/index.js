da5ee330eba70c559f324fa29f6a92048d5243f8
YuanqianFang <richardfang001105@gmail.com>
2022-05-10 17:29:57 +0800

    login token

--- app/src/store/user/index.js
+++ app/src/store/user/index.js
@@ -1,38 +1,52 @@
 //登录注册的模块
-import { reqGetCode, reqUserRegister } from "@/api";
+import { reqGetCode, reqUserLogin, reqUserRegister } from "@/api";
 const state = {
-    code:""
+  code: "",
+  token: "",
 };
-const mutations={
-    GETCODE(state,code){
-        state.code = code
+const mutations = {
+  GETCODE(state, code) {
+    state.code = code;
+  },
+  USERLOGIN(state, token) {
+    state.token = token;
+  },
+};
+const actions = {
+  async getCode({ commit }, phone) {
+    let result = await reqGetCode(phone);
+    if (result.code == 200) {
+      commit("GETCODE", result.data);
+      return "OK";
+    } else {
+      return Promise.reject(new Error("fail"));
     }
-};
-const actions={
-    async getCode({commit},phone){
-        let result = await reqGetCode(phone);
-        if(result.code == 200){
-            commit("GETCODE",result.data)
-            return 'OK'
-        }else{
-            return Promise.reject(new Error('fail'))
-        }
-    },
+  },
 
-    async userRegister({commit},user){
-        let result = await reqUserRegister(user)
-        if(result.code == 200){
-            return 'OK'
-        }else{
-            return Promise.reject(new Error('fail'))
-        }
+  async userRegister({ commit }, user) {
+    let result = await reqUserRegister(user);
+    if (result.code == 200) {
+      return "OK";
+    } else {
+      return Promise.reject(new Error("fail"));
     }
-};
-const getters={}
+  },
 
-export default{
-    state,
-    mutations,
-    actions,
-    getters
-}
+  async userLogin({ commit }, data) {
+    let result = await reqUserLogin(data);
+    if (result.code == 200) {
+      commit("USERLOGIN", result.data.token);
+      return "OK";
+    } else {
+      return Promise.reject(new Error("fail"));
+    }
+  },
+};
+const getters = {};
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+};
